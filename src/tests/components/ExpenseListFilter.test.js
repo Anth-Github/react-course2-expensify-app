e64c94b4d06d789f9824c7f66c28fea86486e8aa
Anthony Prakash <anthonyprakash7@gmail.com>
2022-10-03 14:41:33 +0530

    Setup production webpack build

--- src/tests/components/ExpenseListFilter.test.js
+++ src/tests/components/ExpenseListFilter.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import { shallow } from 'enzyme';
 import { ExpenseListfilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
@@ -70,12 +71,19 @@ test('should sort by amount', () => {
     expect(sortByAmount).toHaveBeenCalled();
 });
 
-test('', () => {
+test('should handle date changes', () => {
+    const startDate = moment(0).add(4, 'years');
+    const endDate = moment(0).add(8, 'years');
+    wrapper.find('DateRangePicker').prop('onDatesChange')({ startDate, endDate });
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);
 
 });
 
-test('', () => {
-
+test('should handle date focus changes', () => {
+    const calendarFocused = 'endDate';
+    wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
 // should handle text change
 // should sort by date
